f0d21d128c94c6a94f9d904499c56841a2d0757a
mshajid <70450142+mshajid@users.noreply.github.com>
2020-11-02 01:39:38 +0530

    02/11 useState API Call for a Function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React, { useState } from "react";
 import './App.css';
+import Tweet from "./Tweet";
+
 
 function App() {
+ 
+  const [users, setUsers] = useState([
+    {name: "Shajid", message: "Hello There"}, //This is the First Object
+    {name: "Madeeha", message: "Hi There"}, //This is the Second Object
+    {name: "Bareera", message: "Hi! Mommy!"}, //This is the third Object
+    {name: "Abrar", message: "Hi, Daddy!"} //This is the Fourth Object
+
+  ]);
+
   return (
-    <div className="App">
-        <h1>This is my React-Basic App</h1>
-    </div>
+        <div className="app">
+          {users.map(user=>(
+            <Tweet name={user.name} message={user.message}/>
+          ))}
+
+
+        </div>
   );
 }
 
---
+++ src/Tweet.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Tweet.css";
+
+function Tweet({name, message}) {
+
+
+    return (
+        <div className="tweet">
+            <h3>{name}</h3>
+            <p>{message}</p>
+            <h3>Number of Likes</h3>
+        </div>
+    )
+}
+
+export default Tweet
